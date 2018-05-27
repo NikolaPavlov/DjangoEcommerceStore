9e9e9d308e4df2db14eb5825762e5e2625c0f931
NikolaPavlov <nikola.pavlov1955@gmail.com>
2018-05-27 15:08:31 +0300

    quickfix

--- webstore/models.py
+++ webstore/models.py
@@ -19,14 +19,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)  # required True
+    name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ManyToManyField('Category')
     image = ImageField(upload_to='img/')
 
     def get_categories(self):
-        # return "\n".join([c.name for c in self.category.all()])
         return ([c for c in self.category.all()])
 
     def __str__(self):
